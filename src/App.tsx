f32afe357afe8c4e9843cbca9b5ab7bc98e9f93c
amanpsharma <14mcaaman@gmail.com>
2023-06-22 13:01:20 +0530

    revert: remove hello function

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,7 @@ import React, { useState } from 'react';
 import './App.css';
 function App() {
   const [count, setCount] = useState(0);
-  const hello_world = () => {
-    return 'hello';
-  };
+
   return (
     <div>
       <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
